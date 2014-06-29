5089427b58acbb3460f1abc59c7e334eba1c1544
yeago <subsume@gmail.com>
2014-06-29 18:52:54 +0200

    voucher notifs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-vouchers',
-    version="0.4",
+    version="0.6",
     author='Federico Castro',
     author_email='fmc0208@gmail.com',
     description='Vouchers in Django',
