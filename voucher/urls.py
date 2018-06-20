3e9f236df698b0c4bec6ab9bc1c83e19021969a9
ivaggione <ivaggione@gmail.com>
2018-06-20 04:23:07 +0000

    Django 1.11.4 urls

--- voucher/urls.py
+++ voucher/urls.py
@@ -1,10 +1,7 @@
-try:
-    from django.conf.urls.defaults import patterns, url  # Django 1.4
-except Exception as e:
-    from django.conf.urls import patterns, url  # Django 1.6
+from django.conf.urls import url
+from voucher import views
 
-urlpatterns = patterns('',
-    #url(r'^$', VoucherList.as_view(), name="voucher_list"),
-    url(r'claim/$', 'voucher.views.claim_voucher', name='claim_voucher'),
-    url(r'claim/(?P<human_token>[-\w]+)$', 'voucher.views.claim_voucher', name='claim_voucher'),
-)
+urlpatterns = [
+    url(r'claim/$', views.claim_voucher, name='claim_voucher'),
+    url(r'claim/(?P<human_token>[-\w]+)$', views.claim_voucher, name='claim_voucher'),
+]
