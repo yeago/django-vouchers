fd8b02b5e1658baf0cd7a9f0da096b93b60fd147
steve yeago <subsume@gmail.com>
2020-01-29 01:23:59 -0500

    Update views.py

--- voucher/views.py
+++ voucher/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import Http404
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
